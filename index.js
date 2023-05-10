84023e91fea76bbba69e0833f15e9dc952522131
phương thanh <you@example.com>
2023-05-10 19:03:52 +0700

    test

--- index.js
+++ index.js
@@ -18,25 +18,57 @@ const { MongoClient, ObjectId } = require('mongodb');
 client = new MongoClient("mongodb://127.0.0.1:27017");
 client.connect();
 database = client.db("ELITE");
-homepageCollection = database.collection("Homepage");
+sliderCollection = database.collection("Slider");
+crewCollection = database.collection("Crew");
 ourstoryCollection = database.collection("OurStory");
 productCollection = database.collection("Product");
 
-/*Get data homepage*/
-app.get("/homepage",cors(),async (req,res)=>{
-    const result = await homepageCollection.find({}).toArray();
+//HOMEPAGE
+// Get data homepage - slider
+app.get("/slider",cors(),async (req,res)=>{
+    const result = await sliderCollection.find({}).toArray();
+    res.send(result)
+    }
+    ) 
+// Get data homepage - crew
+app.get("/crew",cors(),async (req,res)=>{
+    const result = await crewCollection.find({}).toArray();
     res.send(result)
     }
     )
-/*Get data Our Story*/
+// Get data homepage - ourstory
 app.get("/ourstory",cors(),async (req,res)=>{
     const result = await ourstoryCollection.find({}).toArray();
     res.send(result)
     }
-    )
-/*Get Product color*/
-app.get("/product", cors(), async (req, res) => {
-    const result = await productCollection.find({"Color.Name":"Blue"}).toArray();
-    res.send(result);
-    });
-// Truyền đối số
+    ) 
+
+//PRODUCT
+//Get all Product
+app.get("/product",cors(),async (req,res)=>{
+  const result = await productCollection.find({}).toArray();
+  res.send(result)
+  }
+  )
+//Get data Product Detail
+app.get('/product/:id', cors(), async (req, res) => {
+  const id = req.params["id"];
+  const result = await productCollection.find({ ProductID: id }).toArray();
+  res.send(result[0]);
+});
+//collection review
+  reviewCollection = database.collection("Review");
+  app.get("/review", async (req, res) => {
+    await reviewCollection
+      .find({})
+      .toArray()
+      .then((result) => {
+        res.send(result);
+      });
+  });
+  //View product review
+  app.get("/review/:id", cors(), async (req, res) => {
+    var o_id = new ObjectId(req.params["id"]);
+    const result = await reviewCollection.find({ _id: o_id }).toArray();
+    res.send(result[0]);
+  });
